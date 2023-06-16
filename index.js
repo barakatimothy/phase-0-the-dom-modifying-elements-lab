3c861c89782e48a06da8ce1edab825c60c152566
barakatimothy <timothybaraka39@gmail.com>
2023-06-16 19:05:55 -0400

    Completed

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const element = document.createElement("div");
-const ul = document.createElement("ul");
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
 
-for (let i = 0; i < 3; i++) {
-  const li = document.createElement("li");
-  li.textContent = (i + 1).toString();
-  ul.append(li);
-}
+// Set the id attribute of the <h1> element to 'victory'
+newHeader.setAttribute('id', 'victory');
 
-element.append(ul);
+// Set the text content of the <h1> element to "YOUR-NAME is the champion"
+const yourName = "John Doe"; // Replace 'John Doe' with your desired name
+newHeader.textContent = `${yourName} is the champion`;
+
+// Append the <h1> element to the document body (or any other desired location)
+document.body.appendChild(newHeader);
